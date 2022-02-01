df7ec77bf4c9154ad4fc92ac44a28a28164fa400
Evernow <44484725+Evernow@users.noreply.github.com>
2022-02-01 14:24:08 -0500

    Switch user agent to Chrome, add sleep

--- .github/workflows/amd-updater.py
+++ .github/workflows/amd-updater.py
@@ -1,5 +1,5 @@
-import subprocess,json
-URL = r"""curl -f -A 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0' -L https://www.amd.com/en/support/graphics/amd-radeon-6000-series/amd-radeon-6900-series/amd-radeon-rx-6900-xt --referer 'https://www.amd.com/en' | grep -Eo 'https?://\S+?\"' """
+import subprocess,json,time
+URL = r"""curl -f -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' -L https://www.amd.com/en/support/graphics/amd-radeon-6000-series/amd-radeon-6900-series/amd-radeon-rx-6900-xt --referer 'https://www.amd.com/en' | grep -Eo 'https?://\S+?\"' """
 co = subprocess.check_output(URL, shell=True).decode('utf-8')
 dictionary_of_drivers = {}
 for link in co.splitlines():
@@ -10,7 +10,8 @@ for link in co.splitlines():
                 dictionary_of_drivers[comeon] = link
 latest_driver_link = (dictionary_of_drivers[max(dictionary_of_drivers)])
 latest_driver_version = max(dictionary_of_drivers)
-windows_driver_version = r"""wget -U 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0' -qO- """ + "https://www.amd.com/en/support/kb/release-notes/rn-rad-win-" + latest_driver_version.replace('.','-') + """ | sed -e 's/<[^>]*>//g'"""
+time.sleep(30)
+windows_driver_version = r"""wget -U 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' -qO- """ + "https://www.amd.com/en/support/kb/release-notes/rn-rad-win-" + latest_driver_version.replace('.','-') + """ | sed -e 's/<[^>]*>//g'"""
 windows_driver_version_output = subprocess.check_output(windows_driver_version, shell=True).decode('utf-8')
 latest_driver_store_version = ""
 for possible_driver_version in windows_driver_version_output.splitlines():
