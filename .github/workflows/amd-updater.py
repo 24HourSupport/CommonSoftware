4c336f885ed04f03e1932eb5d4479ecb3caa9ba6
Evernow <danielsuarez369@protonmail.com>
2022-02-01 13:42:28 -0500

    Add AMD autoupdater

---
+++ .github/workflows/amd-updater.py
@@ -0,0 +1,34 @@
+import subprocess,json
+URL = r"""curl -f -A 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0' -L https://www.amd.com/en/support/graphics/amd-radeon-6000-series/amd-radeon-6900-series/amd-radeon-rx-6900-xt --referer 'https://www.amd.com/en' | grep -Eo 'https?://\S+?\"' """
+co = subprocess.check_output(URL, shell=True).decode('utf-8')
+dictionary_of_drivers = {}
+for link in co.splitlines():
+    if ".exe" in link and "pro-software" not in link and "minimalsetup" not in link:
+        damnthisshit = (link.split('-'))
+        for comeon in damnthisshit:
+            if comeon.count('.') >= 2 and ".com" not in comeon:
+                dictionary_of_drivers[comeon] = link
+latest_driver_link = (dictionary_of_drivers[max(dictionary_of_drivers)])
+latest_driver_version = max(dictionary_of_drivers)
+windows_driver_version = r"""wget -U 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0' -qO- """ + "https://www.amd.com/en/support/kb/release-notes/rn-rad-win-" + latest_driver_version.replace('.','-') + """ | sed -e 's/<[^>]*>//g'"""
+windows_driver_version_output = subprocess.check_output(windows_driver_version, shell=True).decode('utf-8')
+latest_driver_store_version = ""
+for possible_driver_version in windows_driver_version_output.splitlines():
+    if "Windows Driver Store" in possible_driver_version:
+        latest_driver_store_version = possible_driver_version[possible_driver_version.find("Windows Driver Store Version ")+29:possible_driver_version.find(")")]
+ 
+ 
+print(latest_driver_link)
+print(latest_driver_version)
+print(latest_driver_store_version)
+
+
+with open('amd_gpu.json', 'r+') as f:
+    data = json.load(f)
+    if latest_driver_version > data["consumer"]["version"]:
+        data["consumer"]["version"] = latest_driver_version
+        data["consumer"]["win_driver_version"] = latest_driver_store_version
+        data["consumer"]["link"] = latest_driver_link
+    json.dump(data, f, indent=4)
+    f.truncate()     # remove remaining part
+    print(data)
