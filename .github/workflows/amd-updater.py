9577a6ced1c71b8efb1627f0293f5a6b19a596f9
Evernow <44484725+Evernow@users.noreply.github.com>
2022-02-03 14:55:40 -0500

    Update amd-updater.py

--- .github/workflows/amd-updater.py
+++ .github/workflows/amd-updater.py
@@ -1,29 +1,30 @@
 import subprocess,json,time
+ 
+latest_driver_link = ""
 URL = r"""curl -f -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' -L https://www.amd.com/en/support/graphics/amd-radeon-6000-series/amd-radeon-6900-series/amd-radeon-rx-6900-xt --referer 'https://www.amd.com/en' | grep -Eo 'https?://\S+?\"' """
 co = subprocess.check_output(URL, shell=True).decode('utf-8')
-dictionary_of_drivers = {}
 for link in co.splitlines():
     if ".exe" in link and "pro-software" not in link and "minimalsetup" not in link:
-        damnthisshit = (link.split('-'))
-        for comeon in damnthisshit:
-            if comeon.count('.') >= 2 and ".com" not in comeon:
-                dictionary_of_drivers[comeon] = link
-latest_driver_link = (dictionary_of_drivers[max(dictionary_of_drivers)])
-latest_driver_version = max(dictionary_of_drivers)
+        latest_driver_link = link[:link.find(".exe")+4]
+        break
 time.sleep(30)
-windows_driver_version = r"""curl -f -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' -L https://www.amd.com/en/support/kb/release-notes/rn-rad-win-""" + latest_driver_version.replace('.','-') +  """ --referer 'https://www.amd.com/en'"""
-windows_driver_version_output = subprocess.check_output(windows_driver_version, shell=True).decode('utf-8')
+Driver_URL = r"curl -f -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' -L " + latest_driver_link + " --referer 'https://www.amd.com/en' -o amddriver.exe" 
+ 
+co = subprocess.check_output(Driver_URL, shell=True)
+ 
+z_extract = "7z x amddriver.exe Config/InstallManifest.json" 
+ 
+co = subprocess.check_output(z_extract, shell=True)
+ 
+latest_driver_version = ""
 latest_driver_store_version = ""
-for possible_driver_version in windows_driver_version_output.splitlines():
-    if "Windows Driver Store" in possible_driver_version:
-        latest_driver_store_version = possible_driver_version[possible_driver_version.find("Windows Driver Store Version ")+29:possible_driver_version.find(")")]
+with open("Config/InstallManifest.json") as f:
+    data = json.load(f)
+    latest_driver_version = (data['BuildInfo']['ExternalVersion'])
+    latest_driver_store_version = ((data['Packages']['Package'][0]['Info']['version']))
  
  
-print(latest_driver_link)
-print(latest_driver_version)
-print(latest_driver_store_version)
-
-
+ 
 with open('amd_gpu.json', 'r+') as f:
     data = json.load(f)
     if latest_driver_version > data["consumer"]["version"]:
