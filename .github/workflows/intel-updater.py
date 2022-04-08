c9217323d7f86c588d09393c6ff5749dad8f7072
Evernow <danielsuarez369@protonmail.com>
2022-04-08 17:37:01 -0400

    Fix some bugs

--- .github/workflows/intel-updater.py
+++ .github/workflows/intel-updater.py
@@ -7,7 +7,6 @@ co = subprocess.check_output(URL, shell=True).decode('utf-8')
 for link in co.splitlines():
     if ".zip" in link:
         latest_driver_link = link[:link.find(".zip")+4]
-        latest_driver_version = link[:link.find(".zip")]
         break
 
 print(latest_driver_link)
@@ -18,7 +17,11 @@ Driver_URL = r"curl -f -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit
 co2 = subprocess.check_output(Driver_URL, shell=True)
  
 z_extract = "7z x intel.zip Graphics/iigd_dch.inf" 
- 
+
+co3 = subprocess.check_output(z_extract, shell=True)
+
+z_extract = "7z x readme.txt" 
+
 co3 = subprocess.check_output(z_extract, shell=True)
 
 r = "Graphics/iigd_dch.inf"
@@ -30,11 +33,25 @@ for line in lines:
         ListOfSupportedGPUs.append(line[line.find("DEV_")+4:line.find("DEV_")+8])
 print(ListOfSupportedGPUs) 
 
+
+
+r = "readme.txt"
+with open(r, "r", encoding='utf-16') as reaaaad: # Fuck you Intel with this UTF-16 shit
+	lines = reaaaad.readlines()
+for line in lines:
+    if "Driver Version" in line:
+        latest_driver_version = line[line.find("Driver Version: ")+len("Driver Version: "):].rstrip()
+print(latest_driver_version) 
+
+
+
+
 with open('intel_gpu.json', 'r+') as f:
     data = json.load(f)
-    if version.parse(latest_driver_store_version) > version.parse(data["consumer"]["version"]):
+    if version.parse(latest_driver_version) > version.parse(data["consumer"]["version"]):
         data["consumer"]["version"] = latest_driver_version
         data["consumer"]["link"] = latest_driver_link.replace("zip", "exe")
+        data["consumer"]["SupportedGPUs"] = str(ListOfSupportedGPUs)
         print("Getting MD5")
         data["consumer"]["MD5"] = hashlib.md5(open("intel.zip",'rb').read()).hexdigest()
         print("MD5 got and written")
