80bbb942ffabff9bff0547790d3a6ca5a3587de4
Evernow <danielsuarez369@protonmail.com>
2022-04-16 18:32:12 -0400

    Add SupportedGPUs to NVIDIA

--- .github/workflows/nvidia-updater.py
+++ .github/workflows/nvidia-updater.py
@@ -1,4 +1,4 @@
-import urllib.request, json
+import urllib.request, json, subprocess, os
 from datetime import datetime, timezone
 
 
@@ -18,11 +18,31 @@ for driver in driver_links:
     uf = urllib.request.urlopen(driver_links[driver])
     html = uf.read()
     res = json.loads(html.decode('utf-8'))
-    updated_driver_details[driver] = [float(res["IDS"][0]['downloadInfo']['Version']), res["IDS"][0]['downloadInfo']['DownloadURL']]
+    updated_driver_details[driver] = [float(res["IDS"][0]['downloadInfo']['Version']), res["IDS"][0]['downloadInfo']['DownloadURL'], None]
 
-#print(updated_driver_details)
+def figureoutsupportedgpus(url):
+
+      Driver_URL = r"curl -f -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' -L " + url + " -o nvidiadriver.exe" 
+      co2 = subprocess.check_output(Driver_URL, shell=True)
+      z_extract = "7z x nvidiadriver.exe ListDevices.txt" 
+      co3 = subprocess.check_output(z_extract, shell=True)
+      testlist = []
+      with open("ListDevices.txt") as file:
+        lines = file.readlines()
+        for line in lines:
+            if 'DEV_' in line.upper() and line[line.find('DEV_')+4:line.find('DEV_')+8] not in testlist:
+                testlist.append(line[line.find('DEV_')+4:line.find('DEV_')+8])
+      os.remove('ListDevices.txt') 
+      os.remove("nvidiadriver.exe") 
+      return testlist
 
 
+
+
+for driver in updated_driver_details:
+  updated_driver_details[driver][2] = figureoutsupportedgpus(updated_driver_details[driver][1])
+
+print(updated_driver_details)
 with open('nvidia_gpu.json', 'r+') as f:
     data = json.load(f)
     for driver_in_repo in data:
@@ -30,8 +50,9 @@ with open('nvidia_gpu.json', 'r+') as f:
         if float(data[driver_in_repo]['version']) < updated_driver_details[driver_in_repo][0]:
             data[driver_in_repo]['version'] = str(updated_driver_details[driver_in_repo][0])
             data[driver_in_repo]['link'] = updated_driver_details[driver_in_repo][1]
+            data[driver_in_repo]['SupportedGPUs'] = updated_driver_details[driver_in_repo][2]
             f.seek(0)
     f.seek(0)
     json.dump(data, f, indent=4)
     f.truncate()     # remove remaining part
-    print(data)
+    #print(data)
