7f9df202917fc5cdc17055c42b4c67a9311c5c1f
Evernow <44484725+Evernow@users.noreply.github.com>
2022-05-24 19:29:44 -0400

    Create cleanpciids.py

---
+++ .github/workflows/cleanpciids.py
@@ -0,0 +1,10 @@
+dafile = json.load(open("PCI-IDS.json"))
+keylist = list(dafile.keys())
+for vendor in list(dafile.keys()):
+        if vendor != '8086' and vendor != '1002' and vendor != '10de' and vendor != '121a':
+            # 1002 = AMD ; 8086 = Intel ; 10de = NVIDIA ; 121a = Voodoo (unlikely but I mean.. doesn't hurt?)
+            del dafile[vendor]
+
+
+with open("PCI-IDS.json", "w") as jsonFile:
+        json.dump(dafile, jsonFile)
