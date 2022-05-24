c2733f2732367dc515e61c979ff1d256f43c174b
Evernow <44484725+Evernow@users.noreply.github.com>
2022-05-24 19:29:49 -0400

    Update pci2json.py

--- .github/workflows/pci2json.py
+++ .github/workflows/pci2json.py
@@ -112,14 +112,3 @@ def pci2json(pci_ids, json_out):
 
 if __name__ == "__main__":
     pci2json()
-    dafile = json.load(open("PCI-IDS.json"))
-    keylist = list(dafile.keys())
-    for vendor in list(dafile.keys()):
-        if vendor != '8086' and vendor != '1002' and vendor != '10de' and vendor != '121a':
-            # 1002 = AMD ; 8086 = Intel ; 10de = NVIDIA ; 121a = Voodoo (unlikely but I mean.. doesn't hurt?)
-            del dafile[vendor]
-    print(dafile.keys())
-
-
-    with open("PCI-IDS.json", "w") as jsonFile:
-        json.dump(dafile, jsonFile)
